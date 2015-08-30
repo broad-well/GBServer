45f4e623025f378fc25f0c00f49a677b3124d2de
michaelpeng2002 <michaelpeng2002@icloud.com>
2015-08-29 20:29:21 -0700

    Announce update 2

--- src/com/Gbserver/listener/Announce.java
+++ src/com/Gbserver/listener/Announce.java
@@ -10,19 +10,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.Gbserver.Main;
 
 public class Announce {
-	public static List<String> announcements = Arrays.asList("Contact me about bugs, features, and ranks here: http://mwms.mooo.com/mc/contact.html. (Click on it)", 
-			"Explore the GitHub page of this plugin: https://github.com/michaelpeng2002/GBServer. (Click on it)");
-	private static int i = 0;
+	public static List<String> announcements = Arrays.asList(
+			
+			"Contact me about bugs, features, and ranks here: http://mwms.mooo.com/mc/contact.html. (Click on it)", 
+			"Explore the GitHub page of this plugin: https://github.com/michaelpeng2002/GBServer. (Click on it)"
+			
+			);
 	public static void registerEvents() {
 
-		final String[] ann = (String[]) announcements.toArray();
-		for(i = 0; i < ann.length; i++){
 			Bukkit.getScheduler().scheduleSyncRepeatingTask(JavaPlugin.getPlugin(Main.class), new Runnable() {
 				public void run() {
-					Bukkit.broadcastMessage(ChatColor.BLUE + "Announcement> " + ChatColor.YELLOW + ann[i]);
+					
+					try{
+					Bukkit.broadcastMessage(ChatColor.BLUE + "Announcement> " + ChatColor.YELLOW + announcements.toArray()[0]);
+					
+					}catch(Exception e){
+						Bukkit.broadcastMessage(ChatColor.BLUE + "Error> " + ChatColor.RED + "ERROR! " + e.getMessage());
+					}
 				}
-			}, toTicks(5*i), toTicks(5*ann.length));
-		}
+			}, toTicks(0), toTicks(5*announcements.size()));
+			Bukkit.getScheduler().scheduleSyncRepeatingTask(JavaPlugin.getPlugin(Main.class), new Runnable() {
+				public void run() {
+					
+					try{
+					Bukkit.broadcastMessage(ChatColor.BLUE + "Announcement> " + ChatColor.YELLOW + announcements.toArray()[1]);
+					
+					}catch(Exception e){
+						Bukkit.broadcastMessage(ChatColor.BLUE + "Error> " + ChatColor.RED + "ERROR! " + e.getMessage());
+					}
+				}
+			}, toTicks(1*5), toTicks(5*announcements.size()));
+
 	}
 	
 	private static long toTicks(int minutes){
