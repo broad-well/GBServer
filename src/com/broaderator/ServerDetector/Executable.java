3e76deb0da32e68026850a7420510c7e34f78361
michaelpeng2002 <michaelpeng2002@icloud.com>
2015-09-13 20:45:46 -0700

    Downtime Detector

---
+++ src/com/broaderator/ServerDetector/Executable.java
@@ -0,0 +1,39 @@
+package com.broaderator.ServerDetector;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import javax.net.SocketFactory;
+
+public class Executable {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		boolean isOpen = true;
+		boolean isPosted = false;
+		Socket s = SocketFactory.getDefault().createSocket();
+		while (true) {
+			try {
+				s.setSoTimeout(7000);
+				s.connect(new InetSocketAddress("127.0.0.1", 25565));
+				s.close();
+				isOpen = true;
+			} catch (ConnectException ce) {
+				isOpen = false;
+			}
+			if(!isOpen){
+				if(!isPosted){
+					Process p = Runtime.getRuntime().exec("/home/michael/TTYtter/ttytter -status=UnexpectedDowntime!");
+					System.err.println("I found downtime and posted Twitter for it.");
+					isPosted = true;
+				}
+				
+			}else{
+				isPosted = false;
+			}
+			Thread.sleep(10 * 60 * 1000);
+		}
+	}
+
+}
